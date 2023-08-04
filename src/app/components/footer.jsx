2d6488a4f8fa00a2aa4d1c9b668e5f67d79a4ca2
BandaruCharanKumar <135110649+BandaruCharanKumar@users.noreply.github.com>
2023-08-04 10:04:44 +0530

    Add files via upload

---
+++ src/app/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer(){
+  return(
+    <>
+    <footer class="w-full bottom-0 bg-white p-8">
+      <div class="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-blue-500 text-center md:justify-evenly">
+        <p class="block antialiased font-sans text-base leading-relaxed text-inherit text-center font-normal bg-blue-500 text-white">© 2023 Freebird</p>
+      </div>        
+    </footer>
+    </>
+  )
+}
+
